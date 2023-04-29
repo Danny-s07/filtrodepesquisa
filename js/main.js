08bcae31c9af712a89300020d46661ef9d97f17a
Diego Queiroz <diegiwg@gmail.com>
2023-04-28 21:39:21 -0300

    Adicionando a opção de buscar por múltiplos termos
    1. Agora, caso queria fazer uma busca por dois ou mais pedaços de uma frase, basta separar com espaço.
    
    2. Refatorei o código, para diminuir ao máximo os uso de IF's. E agora, tudo ocorre em um único LOOP.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
+const filterElement = document.querySelector('header input');
+const cards = document.querySelectorAll('.cards li');
 
-const filterElement = document.querySelector('header input')
-const cards = document.querySelectorAll('.cards li')
+filterElement.addEventListener('input', filterCards);
 
-filterElement.addEventListener('input', filterCards)
+function filterCards() {
+  cards.forEach((card) => {
+    // Resetar o estilo do Card
+    card.style.display = 'block';
 
-function filterCards()
-{
-    if(filterElement.value !== ''){
-        for(let card of cards){
-            let title = card.querySelector('h2')
-            title = title.textContent.toLowerCase()
-            let filterText = filterElement.value.toLowerCase()
-            if (!title.includes(filterText)) {
-                card.style.display = "none"
-               
-                
-            }
-            else{
-                card.style.display = "block"
-            }
-        }
+    // Como tem por garantia que sempre terá valor no titulo do Card,
+    // pode pegar direto o H2, sem fazer verificações de segurança antes.
+    const title = card.querySelector('h2').textContent.toLowerCase();
+    const filterText = filterElement.value.toLowerCase();
+
+    // Para dar suporte simples a multi-termos, você pode usar a aborgadem
+    // de ter uma lista, e caso a palavra exista no titulo, salvar um true
+    // e no final, caso tenha no minimo um true, mostrar o card
+    const showCard = [];
+    for (let word of filterText.split(' ')) {
+      showCard.push(title.includes(word));
     }
-    else{
-        for (let card of cards) {
-            card.style.display = "block"
-        }
-    }
+
+    card.style.display = showCard.includes(true) ? 'block' : 'none';
+  });
 }
